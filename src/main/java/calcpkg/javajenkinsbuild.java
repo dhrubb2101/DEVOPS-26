9cc50f0c2398a7fe0ae800bb27a38540871bdaca
dhrubb <dhrubbgupta@gmail.com>
2024-09-29 23:12:41 +0530

    Final Project (eclipse Program)

--- src/main/java/calcpkg/javajenkinsbuild.java
+++ src/main/java/calcpkg/javajenkinsbuild.java
@@ -12,11 +12,31 @@ public class javajenkinsbuild {
 		return a-b;
 	}
 	
+	public int mul(int a,int b)
+	{
+		return a*b;
+	} 	
+	
+	public int div(int a,int b)
+	{
+		return a/b;
+	}
+	
+	public void greeting(){
+		
+		System.out.println("Hello GFG");
+		
+		
+	}
+	
 	public static void main(String[] args) {
 		
 		javajenkinsbuild ob = new javajenkinsbuild();
 		System.out.println("Sum is: "+ob.sum(20,10));
 		System.out.println("Difference is: "+ob.diff(20,10));
+		System.out.println("Multiplication is: "+ob.mul(20,10));
+		System.out.println("Division is: "+ob.div(20,10));
+
 	}
 
 }
